d7aa7782012151762680847a3316df6acd58150e
GalinaHussey <hello@balletgalina.com>
2024-05-20 21:58:07 +1000

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '9e2d10ec-813c-4310-9843-7e4604c17f65';
+export const MODAL_CLOSE_SEC = 2.5;
